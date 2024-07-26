111f7f29b9b4b103cb6d0a757345e020c1444174
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2024-07-26 11:39:43 +0700

    save image use preferences

--- src/app/pages/tab4/tab4.page.ts
+++ src/app/pages/tab4/tab4.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-tab4',
@@ -10,35 +11,55 @@ import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 export class Tab4Page implements OnInit {
 
   user: any = {
-    name: 'Damarazks Akhyari',
+    name: 'Damarazka Akhyari',
     email: 'example123@mail.com',
     phone: '123-456-7890',
     instagram: 'ahoha_',
     address: '123 Main St, Anytown, USA',
-    avatar: 'assets/ciput.jpeg'
+    avatar: ''
+  };
+
+  constructor(private router: Router) { }
+
+  async ngOnInit() {
+    await this.loadAvatar()
   }
 
-  constructor(private router : Router) { }
-
-  ngOnInit() {
+  goToBookmark() {
+    this.router.navigateByUrl('/bookmark');
   }
 
-  goToBookmark(){
-    this.router.navigateByUrl('/bookmark')
-  }
-
-  goToHistory(){
-    this.router.navigateByUrl('/history')
+  goToHistory() {
+    this.router.navigateByUrl('/history');
   }
 
   async changeAvatar() {
-    const image = await Camera.getPhoto({
-      quality: 90,
-      allowEditing: true,
-      resultType: CameraResultType.DataUrl,
-      source: CameraSource.Prompt
-    });
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Prompt
+      });
 
-    this.user.avatar = image.dataUrl;
+      if (image.dataUrl) {
+        this.user.avatar = image.dataUrl;
+        await Preferences.set({
+          key: 'userAvatar',
+          value: image.dataUrl
+        });
+      } else {
+        console.error('Failed to capture image data URL.');
+      }
+    } catch (error) {
+      console.error('Failed to capture image:', error);
+    }
+  }
+
+  async loadAvatar() {
+    const { value } = await Preferences.get({ key: 'userAvatar' });
+    if (value) {
+      this.user.avatar = value;
+    }
   }
 }
