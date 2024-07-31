a864fb49d30d040073613ce01e8efdbe25a9f116
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2024-07-31 14:51:43 +0700

    login register

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  username!: string
+  password!: string
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  async login(){
+    const {value} = await Preferences.get({key: this.username})
+    const user = value ? JSON.parse(value): null
+
+    if (user && user.password === this.password) {
+      alert('login sukses')
+      this.router.navigateByUrl('/tabs/tab1')
+    }else{
+      alert('invalid credential')
+    }
   }
 
 }
