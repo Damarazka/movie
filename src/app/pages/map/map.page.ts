c353be75ef2b30f6c7d2c29103a62c416716531b
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2024-08-02 16:04:51 +0700

    get LngLat

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import mapboxgl from 'mapbox-gl';
 
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
@@ -8,6 +9,8 @@ import mapboxgl from 'mapbox-gl';
 })
 export class MapPage implements OnInit {
   map!: mapboxgl.Map
+  markers: mapboxgl.Marker[] = []
+
   constructor() { }
 
   ngOnInit() {
@@ -18,19 +21,41 @@ export class MapPage implements OnInit {
     (mapboxgl as any).accessToken = 'pk.eyJ1IjoiaXRzaGF6bGFuIiwiYSI6ImNsMXZsZDVuejA3dWszY29ocWU3dGQ0bGYifQ.sOxTgUHtJzDLkSk4fKLocg'; // Ganti dengan akses token Anda
 
     this.map = new mapboxgl.Map({
-      container: 'map', // ID elemen HTML tempat peta akan ditampilkan
-      style: 'mapbox://styles/mapbox/streets-v11', // Gaya peta
-      center: [106.865036, -6.175110], // Koordinat pusat peta (longitude, latitude)
-      zoom: 12 // Tingkat zoom awal
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [106.865036, -6.175110],
+      zoom: 12
     });
 
-    // Menambahkan kontrol navigasi (zoom dan rotasi)
+    
     this.map.addControl(new mapboxgl.NavigationControl());
 
-    // Menambahkan marker ke peta
-    new mapboxgl.Marker()
-      .setLngLat([106.865036, -6.175110])
-      .setPopup(new mapboxgl.Popup().setHTML('<h3>Monas</h3><p>Monumen Nasional</p>')) // Menambahkan popup pada marker
+    this.map.on('click', (event) => {
+      this.addMarker(event.lngLat)
+      this.updateCoordinate(event.lngLat)
+    })
+  }
+
+  addMarker(lngLat: mapboxgl.LngLat){
+    this.markers.forEach(marker => marker.remove())
+    this.markers = []
+
+    const marker = new mapboxgl.Marker()
+      .setLngLat(lngLat)
+      .setPopup(new mapboxgl.Popup().setHTML(`<h3>New Marker</h3><p>Latitude: ${lngLat.lat}, Longitude: ${lngLat.lng}</p>`))
       .addTo(this.map);
+
+    this.markers.push(marker);
+  }
+
+  updateCoordinate(lngLat: mapboxgl.LngLat){
+    const latitudeElem = document.getElementById('latitude') 
+    const longitudeElem = document.getElementById('longitude') 
+
+    if (latitudeElem && longitudeElem) {
+      latitudeElem.textContent = lngLat.lat.toFixed(6)
+      longitudeElem.textContent = lngLat.lng.toFixed(6)
+    }
+    console.log(`Latitude: ${lngLat.lat.toFixed(6)}, Longitude: ${lngLat.lng.toFixed(6)}`);
   }
 }
